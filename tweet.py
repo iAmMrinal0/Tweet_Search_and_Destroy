da952363ee0fd58ea9c79a02b498a205c407c0c1
Mrinal Purohit <mrinal1892005@gmail.com>
2015-12-10 23:40:31 +0530

    Flask and Twitter OAuth with keyword search
    Search for a keyword in the last 200 tweets of the authenticated user.

--- tweet.py
+++ tweet.py
@@ -1,8 +1,88 @@
+from flask import Flask
+from flask import g, session, request, url_for, flash
+from flask import redirect, render_template
+from flask_oauthlib.client import OAuth
 import os
-import tweepy
 
-auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
 
-api = tweepy.API(auth)
-user = api.get_user("iammrinal0")
-print(user.screen_name)
+oauth = OAuth(app)
+
+twitter = oauth.remote_app(
+    'twitter',
+    consumer_key=os.environ["CONSUMER_KEY"],
+    consumer_secret=os.environ["CONSUMER_SECRET"],
+    base_url='https://api.twitter.com/1.1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+)
+
+
+@twitter.tokengetter
+def get_twitter_token():
+    if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'twitter_oauth' in session:
+        g.user = session['twitter_oauth']
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    if g.user is None:
+        return redirect(url_for('login', next=request.url))
+    search_word = request.form['tweet']
+    if not search_word:
+        return redirect(url_for('index'))
+
+    resp = twitter.request('statuses/user_timeline.json?count=200')
+    found = []
+    all_tweets = resp.data
+
+    for twit in all_tweets:
+        if search_word.lower() in twit["text"].lower():
+            found.append(twit)
+
+    if resp.status == 401:
+        flash('Authorization error with Twitter.')
+
+    return render_template('search.html', tweets=found)
+
+
+@app.route('/login')
+def login():
+    callback_url = url_for('oauthorized', next=request.args.get('next'))
+    return twitter.authorize(callback=callback_url or request.referrer or None)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('twitter_oauth', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/oauthorized')
+def oauthorized():
+    resp = twitter.authorized_response()
+    if resp is None:
+        flash('You denied the request to sign in.')
+    else:
+        session['twitter_oauth'] = resp
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
